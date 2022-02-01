09c48428a50bc9d0cf98be7ce8572d6636f7989d
marianafocassio <focassiomariana@gmail.com>
2022-01-31 21:45:52 -0300

    adcionando footer

--- lancamentos/javascript-lancamentos.js
+++ lancamentos/javascript-lancamentos.js
@@ -9,3 +9,9 @@ function toggleMenu(){
 }
 
 btnMobile.addEventListener('click', toggleMenu);
+
+var btn = document.querySelector("#back-to-top");
+
+btn.addEventListener("click", function() {
+    window.scrollTo(0, 0);
+});
